1c3082c397cc6dee33109091e2354bb5967f188a
Dmytro Kolomiiets <dmkolomiec@gmail.com>
2023-02-16 17:56:07 +0200

    localdate

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+//import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDateTime ld = LocalDateTime.now();
+//        LocalDateTime ld = LocalDateTime.now();
+        LocalDate ld = LocalDate.now();
         System.out.printf("Local Date: %d.%d.%d (%s)\n",ld.getDayOfMonth(),ld.getMonthValue(),ld.getYear(),ld.getDayOfWeek());
     }
 }
